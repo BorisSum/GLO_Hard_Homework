819d0900cfbf809f0baac254cd1906664a0893cb
Boris Sumarokov <bor1999@mail.ru>
2020-11-20 19:30:16 +0300

    Усложненное задание Урок 2

---
+++ script.js
@@ -0,0 +1,21 @@
+// ------------ 1. ------------------------------------
+const num = 266219;
+
+// ------------ 2. ------------------------------------
+const DigitsOfNum = num.toString().split(''); // array of strings (chars)
+let ProductOfDigits = +DigitsOfNum[0];
+
+for (let i = 1; i < DigitsOfNum.length; i++) {
+   ProductOfDigits *= +DigitsOfNum[i];
+}
+console.log(ProductOfDigits);
+
+// ------------ 3. ------------------------------------
+ProductOfDigits **= 3;
+
+// ------------ 4. ------------------------------------
+
+const result = document.createElement('h1');
+document.body.appendChild(result);
+result.textContent = ProductOfDigits.toString();
+
