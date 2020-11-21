b5f53676b4346d1a08948450dc0550761aefbcfb
Boris Sumarokov <bor1999@mail.ru>
2020-11-21 10:54:01 +0300

    Использовал forEach вместо цикла, добавил use strict

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+'use strict';
+
 // ------------ 1. ------------------------------------
 const num = 266219;
 
 // ------------ 2. ------------------------------------
 const DigitsOfNum = num.toString().split(''); // array of strings (chars)
-let ProductOfDigits = +DigitsOfNum[0];
+//let ProductOfDigits = +DigitsOfNum[0];
+let ProductOfDigits = 1;
 
-for (let i = 1; i < DigitsOfNum.length; i++) {
-   ProductOfDigits *= +DigitsOfNum[i];
-}
-console.log(ProductOfDigits);
+// for (let i = 1; i < DigitsOfNum.length; i++) {
+//    ProductOfDigits *= +DigitsOfNum[i];
+// }
+//console.log(ProductOfDigits);
+
+DigitsOfNum.forEach(item => { ProductOfDigits *= +item; });
+console.log('Произведение всех цифр числа: ', ProductOfDigits);
 
 // ------------ 3. ------------------------------------
 ProductOfDigits **= 3;
@@ -18,4 +24,3 @@ ProductOfDigits **= 3;
 const result = document.createElement('h1');
 document.body.appendChild(result);
 result.textContent = ProductOfDigits.toString().slice(0, 2);
-
