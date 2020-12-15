99eab1e659aa281118e790cc3ab96f4bbde6e0b8
Boris Sumarokov <bor1999@mail.ru>
2020-12-15 20:00:11 +0300

    Урок 18 Допзадание 2

---
+++ script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', () => {
+   'use strict';
+
+   let coordX = 0,
+      coordY = 0,
+      requestID;
+
+   const btnPause = document.querySelector('.pause');
+   const btnReset = document.querySelector('.reset');
+
+   const createElement = () => {
+      const elem = document.createElement('div');
+      elem.style.cssText = `position:relative; left:0; top:0; background-color:blue; width:100px; height:100px; margin-top:50px`;
+      document.body.append(elem);
+      return elem;
+   };
+
+   const element = createElement();
+
+   const animation = () => {
+      requestID = requestAnimationFrame(animation);
+      coordX++;
+
+      if (coordX < 600) {
+         element.style.left = coordX + 'px';
+      } else {
+         cancelAnimationFrame(requestID);
+      }
+   };
+
+   let animationFlag = false;
+
+   btnPause.addEventListener('click', event => {
+      event.preventDefault();
+      if (!animationFlag) {
+         cancelAnimationFrame(requestID);
+         animationFlag = true;
+      } else {
+         animation();
+         animationFlag = false;
+      }
+   });
+
+   btnReset.addEventListener('click', event => {
+      event.preventDefault();
+      cancelAnimationFrame(requestID);
+      coordX = 0;
+      element.style.left = coordX + 'px';
+      animationFlag = false;
+      animation();
+
+   });
+
+   animation();
+});
